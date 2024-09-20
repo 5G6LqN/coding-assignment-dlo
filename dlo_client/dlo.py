0f28d8dc8bc713d7899cb007ed504ebca0b1ebc7
Dirk Geschwandtner <digesch@gmail.com>
2024-09-20 13:54:08 +0200

    Multiple changes to dlo.py
    - Introduce enum for usertype, documentation, reordering
    - Changes to allow tests overwrite secret key
    
    DLO Anpassungen

--- dlo_client/dlo.py
+++ dlo_client/dlo.py
@@ -1,47 +1,30 @@
-import hmac
 import hashlib
+import hmac
 import os
+import time
 import uuid
-from datetime import datetime
+from enum import StrEnum
+from urllib.parse import urlencode
 
 from pydantic import BaseModel
-from urllib.parse import urlencode
-import time
 
 BASE_URL = "https://ca-diroiaya.minddistrict.dev/"
 
 
+class UserType(StrEnum):
+    PROFESSIONAL = "careprovider"
+    CLIENT = "client"
+
+
 class DLORequest(BaseModel):
     user_id: str
-    usertype: str
+    usertype: UserType
     redirect: str = None
-    shared_secret_key: str = os.environ['SHARED_KEY_SECRET']
+    shared_secret_key: str = None
 
-    @staticmethod
-    def _generate_hmac_token(params: dict, secret_key: str):
-        """
-        Generate an HMAC token for DLO using the secret key provided.
-
-        Args:
-            params: Dictionary of parameters to be hashed
-            secret_key: The shared secret key
-
-        Returns:
-            str: The HMAC token
-        """
-        message = ''.join(f'{k}{v}' for k, v in sorted(params.items()))
-        return hmac.new(secret_key.encode(), message.encode(), hashlib.sha512).hexdigest()
-
-    @staticmethod
-    def _generate_nonce() -> str:
-        """
-        Generate a random nonce.
-
-        Returns:
-            str: The generated nonce
-        """
-        return uuid.uuid4()
-
+    def __init__(self, user_id, usertype):
+        super().__init__(user_id=user_id, usertype=usertype)
+        self.shared_secret_key = os.environ['SHARED_KEY_SECRET']
 
     def generate_dlo_url(self) -> str:
         """
@@ -69,3 +52,28 @@ class DLORequest(BaseModel):
         # Generate the final URL
         dlo_url = f"{BASE_URL}?{urlencode(params)}"
         return dlo_url
+
+    @staticmethod
+    def _generate_nonce() -> str:
+        """
+        Generate a random nonce.
+
+        Returns:
+            str: The generated nonce
+        """
+        return uuid.uuid4()
+
+    @staticmethod
+    def _generate_hmac_token(params: dict, secret_key: str):
+        """
+        Generate an HMAC token for DLO using the secret key provided.
+
+        Args:
+            params: Dictionary of parameters to be hashed
+            secret_key: The shared secret key
+
+        Returns:
+            str: The HMAC token
+        """
+        message = ''.join(f'{k}{v}' for k, v in sorted(params.items()))
+        return hmac.new(secret_key.encode(), message.encode(), hashlib.sha512).hexdigest()
--- dlo_client/views.py
+++ dlo_client/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 
 from django.http import HttpResponseRedirect
 
-from dlo_client.dlo import DLORequest
+from dlo_client.dlo import DLORequest, UserType
 
 
 def index(request):
     return render(request, 'index.html')
 def my_view(request):
-    dlo_request = DLORequest(user_id="dirk+professional", usertype="careprovider")
+    dlo_request = DLORequest(user_id="dirk+professional", usertype=UserType.PROFESSIONAL)
     target_url = dlo_request.generate_dlo_url()
 
     return HttpResponseRedirect(target_url)
